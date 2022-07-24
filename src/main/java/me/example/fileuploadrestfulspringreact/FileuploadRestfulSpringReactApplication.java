6173f1ae8364cc884024492ff22d718a55d986fa
leejongwoo <leejongwoo.dev@gmail.com>
2022-07-24 09:55:19 +0900

    Created Project

---
+++ src/main/java/me/example/fileuploadrestfulspringreact/FileuploadRestfulSpringReactApplication.java
@@ -0,0 +1,13 @@
+package me.example.fileuploadrestfulspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadRestfulSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuploadRestfulSpringReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/example/fileuploadrestfulspringreact/FileuploadRestfulSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package me.example.fileuploadrestfulspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadRestfulSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
